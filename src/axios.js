9bb60d00980c5f8f49b968f733b375fe07093958
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2023-11-04 14:50:36 +0100

    cors

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import store from "./store";
-
+// barryvdh/laravel-cors
 const axiosClient = axios.create({
      baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+     withCredentials: true,
 });
 axiosClient.interceptors.request.use((config) => {
      config.headers.Authorization = `Bearer ${store.state.user.token}`;
